f3c4965216888329eb03b5ef169f28082e81c53e
jibola <ajibolaajiboye99@gmail.com>
2023-09-03 16:45:24 +0100

    first commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='flex w-full'>
+<p className='text-red-400'>dhdhdhd</p>
+<p className='text-blue-4'>oooooooo</p>
+    </div>
+  )
+}
+
+export default LoginPage
